bd9ef90a78d9e62d5be2a8e053b2767455830705
kevin zhang <kevzyzhang@gmail.com>
2026-01-12 23:07:37 -0500

    got basic projects tab runnning

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 PROJECTS = [
-    {"title": "Project", "description": "description"},
+    {
+        "title": "Banana phone", 
+        "description": "This is a little python file that i made in october 2025. Every once in a while, I pretend to lose my phone and have my girlfriend call it before taking a banana out of my back pocket and answering \"yellow?\" It keeps her on her toes. We go to different schools but despite the distance, she can now get hit with this epic prank.",
+        "image": "images/bananapjone.jpg",
+        "url": "https://github.com/kz357/banana-phone-sensor/tree"
+    },
 ]
 
 @app.route("/")
