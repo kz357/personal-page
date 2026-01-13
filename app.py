e0245182e4a38673908ab175586d79bb3fc4e8d5
kevin zhang <kevzyzhang@gmail.com>
2026-01-13 00:26:53 -0500

    projects link underscore

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 PROJECTS = [
     {
         "title": "Banana phone", 
-        "description": "This is a little python file that i made in october 2025. I like to pretend to lose my phone and have my girlfriend call it before pulling out a and saying \"yellow?\" I like to keep her on her toes. We go to different schools now but despite the distance, she can still stimulate this epic prank. Most important parts are mediapipe and ultralytics YOLOv8",
+        "description": "This is a little python file that i made in october 2025. I like to pretend to lose my phone and have my girlfriend call it before pulling out a and saying \"yellow?\" I like to keep her on her toes. We go to different schools now but despite the distance, she can still stimulate this epic prank. Most important libraries are mediapipe and ultralytics YOLOv8",
         "image": "images/bananapjone.jpg",
         "url": "https://github.com/kz357/banana-phone-sensor"
     },
